0c0945c525bd796ceba619a728901a7ebc37aaa9
meijiasong <1913549290@gmail.com>
2017-11-15 07:38:47 +0000

    redis

--- config/database.php
+++ config/database.php
@@ -107,7 +107,10 @@ return [
     'redis' => [
 
         'client' => 'predis',
-
+        'driver' => 'redis',
+        'connection' => 'default',
+        'queue' => '{default}',
+        'retry_after' => 90,
         'default' => [
             'host' => env('REDIS_HOST', '127.0.0.1'),
             'password' => env('REDIS_PASSWORD', null),
